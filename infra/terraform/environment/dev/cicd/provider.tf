0a722d6d06e91d229761021da237d6a7f260af26
Ethan <63649819+kuma0128@users.noreply.github.com>
2025-06-25 22:40:39 +0900

    Add Terragrunt configs for backend and cicd

--- infra/terraform/environment/dev/cicd/provider.tf
+++ infra/terraform/environment/dev/cicd/provider.tf
@@ -1,40 +1,5 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0.0"
-    }
-
-    http = {
-      source  = "hashicorp/http"
-      version = "~> 3.5.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.7.2"
-    }
-  }
-  # backend "s3" {
-  #   bucket         = "ethan-s3-apne1-tfstate"
-  #   region         = "ap-northeast-1"
-  #   key            = "ethan/dev/cicd/remote.tfstate"
-  #   use_lockfile   = true
-  # }
-  backend "local" {
-    path = "local.tfstate"
-  }
+  # Backend is left empty so Terragrunt can configure it.
+  backend "s3" {}
   required_version = "~> 1.12.2"
-}
-
-provider "aws" {
-  # Configuration options
-}
-
-provider "http" {
-  # Configuration options
-}
-
-provider "random" {
-  # Configuration options
 }
---
+++ infra/terragrunt/environment/dev/backend/terragrunt.hcl
@@ -0,0 +1,41 @@
+terraform {
+  source = "../../../../..//infra/terraform/environment/dev/backend"
+}
+
+remote_state {
+  backend = "s3"
+  config = {
+    bucket = "ethan-s3-apne1-tfstate"
+    key    = "ethan/dev/backend/remote.tfstate"
+    region = "ap-northeast-1"
+    encrypt = true
+  }
+}
+
+generate "provider" {
+  path = "generated_provider.tf"
+  if_exists = "overwrite"
+  contents = <<EOT
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0.0"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
+  }
+  required_version = "~> 1.12.2"
+}
+
+provider "aws" {}
+provider "http" {}
+provider "random" {}
+EOT
+}
