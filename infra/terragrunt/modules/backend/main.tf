981e11d70935663dae929b0b8f83a34066a92bad
Ethan <63649819+kuma0128@users.noreply.github.com>
2025-06-26 17:26:24 +0900

    Enable auto S3 backend creation

--- infra/terragrunt/modules/backend/main.tf
+++ infra/terragrunt/modules/backend/main.tf
@@ -1,20 +1,13 @@
-resource "aws_s3_bucket" "this" {
+data "aws_s3_bucket" "this" {
   bucket = "${var.name_prefix}-s3-${var.region_short_name}-tfstate"
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-s3-${var.region_short_name}-tfstate"
-  }
 }
 
 resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
+  bucket = data.aws_s3_bucket.this.id
   policy = templatefile(
     "${path.module}/bucket_policy/backend.json.tpl",
     {
-      s3_bucket_arn     = aws_s3_bucket.this.arn
+      s3_bucket_arn     = data.aws_s3_bucket.this.arn
       allowed_aws_roles = var.github_actions_iam_role_id
       allowed_vpce_ids  = var.allowed_vpce_ids
       allowed_ips       = var.allowed_ip_list
@@ -23,7 +16,7 @@ resource "aws_s3_bucket_policy" "this" {
 }
 
 resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
+  bucket = data.aws_s3_bucket.this.id
 
   versioning_configuration {
     status = "Enabled"
@@ -31,7 +24,7 @@ resource "aws_s3_bucket_versioning" "this" {
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
-  bucket                  = aws_s3_bucket.this.id
+  bucket                  = data.aws_s3_bucket.this.id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
