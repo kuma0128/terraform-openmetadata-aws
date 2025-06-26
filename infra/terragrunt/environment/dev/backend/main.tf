61ff4953d197b746f4514de208372a21f26ae1b2
Ethan <63649819+kuma0128@users.noreply.github.com>
2025-06-26 20:29:23 +0900

    Use consistent bucket name

--- infra/terragrunt/environment/dev/backend/main.tf
+++ infra/terragrunt/environment/dev/backend/main.tf
@@ -3,8 +3,9 @@ terraform {
 }
 
 module "backend" {
-  source                     = "../../../modules/backend"
-  name_prefix                = "${var.pj_name}-${var.env}"
+  source = "../../../modules/backend"
+  # Use project name only so the backend bucket matches the global state bucket
+  name_prefix                = var.pj_name
   region_short_name          = var.region_short_name
   allowed_ip_list            = var.allowed_ip_list
   github_actions_iam_role_id = var.github_actions_iam_role_id
