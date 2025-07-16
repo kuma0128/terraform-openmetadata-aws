dc5417ef899a03e9f5b164d4ef0d3fd69640c4f7
Ethan <63649819+kuma0128@users.noreply.github.com>
2025-07-17 04:43:37 +0900

    Update tfsec workflow and restrict SG egress

--- infra/terragrunt/modules/aws/network/sg.tf
+++ infra/terragrunt/modules/aws/network/sg.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "self_reference" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.cidr_vpc]
   }
 
   tags = {
